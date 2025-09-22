9236b06e74d30e08a0e5f07bc7f9b5983ad17416
Jakub-Stepien1 <ninjadeluxepl@gmail.com>
2025-09-22 11:23:30 +0100

    added pages for each feature

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+<button><a href="/home">Home</a></button>
+<button><a href="/map">Map</a></button>
+<button><a href="/tasks">Tasks</a></button>
+<button><a href="/shop">Shop</a></button>
+<button><a href="/avatar">Avatar</a></button>
+<button><a href="/settings">Settings</a></button>
